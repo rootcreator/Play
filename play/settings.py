2158751a3e0117f6cc6485e877e52eaa68d68f34
Marcelinus Apakhade <maarrxxy@gmail.com>
2023-12-26 15:32:42 +0100

    v3
    intergrating posgresql

--- play/settings.py
+++ play/settings.py
@@ -95,10 +95,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Root101.com',
+        'HOST': 'localhost',  # Or your PostgreSQL server IP
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
 
