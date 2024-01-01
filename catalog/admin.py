bb5ac5a69b0de82a672bd344ba3c76c515613df6
Marcelinus Apakhade <maarrxxy@gmail.com>
2024-01-01 03:49:15 +0100

    V1.2
    Modificatitions

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Genre, Artist, Album, Song
+from .models import Genre, Artist, Album, Song, Composer
 
 admin.site.register(Genre)
 admin.site.register(Artist)
-
+admin.site.register(Composer)
 admin.site.register(Album)
 admin.site.register(Song)
